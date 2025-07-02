154c30ced59b38558354cffa64d626dbe6716810
AzureAD\LokeshR <lokesh.r@kubernetikos-infosec.com>
2025-07-02 23:14:18 +0530

    added docker

--- app.js
+++ app.js
@@ -75,6 +75,6 @@ app.get('/api/test', (req, res) => {
 
 // Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
